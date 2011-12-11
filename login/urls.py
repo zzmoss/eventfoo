af61c6f90b8ab19b57157dd7378b6579172b0424
Madhumitha Viswanathan <madhuvishy@gmail.com>
2011-12-12 01:33:17 +0530

    Stash conflict resolved in login/urls.py

--- login/urls.py
+++ login/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$','login.views.index'),
-<<<<<<< Updated upstream
-    (r'^register','login.views.register'),
-=======
 
->>>>>>> Stashed changes
+    (r'^register','login.views.register'),
+
+
     )
